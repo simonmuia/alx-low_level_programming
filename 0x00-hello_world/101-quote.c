a5a08b4809ebcc52ee20a44842b83a671308f910
simonmuia <simonmuia38@gmail.com>
2023-02-16 07:26:41 -0800

    Print unix program

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
 /**
  * main - C program that prints line to std error
  * Return:1 (Successful)
@@ -7,6 +6,6 @@
 
 int main(void)
 {
-fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
 return (1);
 }
