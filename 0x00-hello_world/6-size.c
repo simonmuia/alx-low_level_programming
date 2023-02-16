0bbeaff4eb65df9e595c323da9aa1a5897d4901a
simonmuia <simonmuia38@gmail.com>
2023-02-16 03:57:38 -0800

    Print sizes of vars

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,14 @@
+#includes <stdio.h>
+/**
+ * main -prints size of input
+ * Return: 0 is successful
+ */
+int main(void)
+{
+	printf("Size of a char: %s\n", char);
+	printf("Size of an int: %d\n", int);
+	printf("Size of a long int: %li\n", long - int);
+	printf("Size of a long long-int: %lli\n", long - long - int);
+	printf("Size of a float: %f", float);
+	return (0);
+}
