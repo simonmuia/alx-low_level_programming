cd366ab3fe302d6e3f3de3feb38bee9be0d0851b
simonmuia <simonmuia38@gmail.com>
2023-02-16 03:12:37 -0800

    print a string of chars

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - prints a string n followed by new line
+ * Return: 0 if successful
+ */
+int main(void){
+	puts("with proper grammar, but the outcome is a piece of art,");
+	return(0);
+}
