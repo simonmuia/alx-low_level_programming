58ff048d5701bfc95e3b5435b398fdd45b06c33f
simonmuia <simonmuia38@gmail.com>
2023-02-16 02:26:35 -0800

    C Program to pring string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+//create function put
+void puts(){
 
-int puts(){
 	printf("Programming is like building a multilingual puzzle\n");
+	return(0);
 }
 
 int main(void){
-	puts()
+	puts(); //call function put
 	return(0);
 }
