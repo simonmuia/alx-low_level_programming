fda099ffb959099d38d38f9a01a7dd740a14652c
simonmuia <simonmuia38@gmail.com>
2023-02-16 06:15:33 -0800

    print size of vars

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+
 /**
  * main -prints size of input
  * types
  * Return: 0 is successful
  */
+
 int main(void)
 {
 	char a;
@@ -15,7 +17,7 @@ int main(void)
 	printf("Size of a char: %zu byte(s)\n", sizeof(a));
 	printf("Size of an int: %zu byte(s)\n", sizeof(b));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(c));
-	printf("Size of a long long-int: %zu byte(s)\n", sizeof(d));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(d));
 	printf("Size of a float: %zu byte(s)\n", sizeof(e));
 	return (0);
 }
