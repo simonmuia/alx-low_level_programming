b28e9589f85cef930096de2221076c4abc8c0cf3
simonmuia <simonmuia38@gmail.com>
2023-02-16 07:04:03 -0800

    Print unix program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - C program that prints line to std error
+ * Return:1 (Successful)
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stderr);
+	return (1)
+}
