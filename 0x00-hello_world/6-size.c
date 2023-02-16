413c564cb4642f1d4cf89b1cdd99608cbeff2bbc
simonmuia <simonmuia38@gmail.com>
2023-02-16 04:41:27 -0800

    print size of vars

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("Size of a char: %s byte(s) \n", sizeof(char));
+	printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %ld byte(s) \n", sizeof(int));
-	printf("Size of a long long-int: %lld byte(s) \n", sizeof(int));
-	printf("Size of a float: %ld byte(s) \n", sizeof(float));
+	printf("Size of a long int: %lu byte(s) \n",(unsigned long)sizeof(int));
+	printf("Size of a long long-int: %lu byte(s) \n",(unsigned long long)sizeof(int));
+	printf("Size of a float: %lu byte(s) \n",(unsigned long) sizeof(float));
 	return (0);
 }
