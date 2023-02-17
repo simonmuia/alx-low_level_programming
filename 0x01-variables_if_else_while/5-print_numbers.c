8b75b1ffd65462dc73ef30ec7193f10b8d0a7d22
simonmuia <simonmuia38@gmail.com>
2023-02-17 02:53:06 -0800

    Print single numbers on base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - entry point
+ * Return:0 (successful)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf(log10(n));
+	}
+	printf('\n');
+	return (0);
+}
