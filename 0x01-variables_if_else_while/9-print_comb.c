3368893d6c2a3c81e191232dca49df04db9229f6
simonmuia <simonmuia38@gmail.com>
2023-02-17 04:04:21 -0800

    print single numbers with , on each

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,12 @@ int main(void)
 
 	for (num = 0; num < 10; num++)
 	{
-		putchar(num + '0');
-		putchar(',');
-		putchar(' ');
+		if(num != 9)
+		{
+			putchar(num + '0');
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
