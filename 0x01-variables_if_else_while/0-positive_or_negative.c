8502e9de0ec83c8f6b642ac4134e338806e0165e
simonmuia <simonmuia38@gmail.com>
2023-02-17 01:37:17 -0800

    Check random number value

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,15 +15,15 @@ int main(void)
 	/* check if the number is >0, < 0 or is zero */
 	if (n > 0)
 	{
-		printf("is positive\n");
+		printf("%d is positive\n", n);
 	}
 	else if (n < 0)
 	{
-		printf("is negative \n");
+		printf("%d is negative \n", n);
 	}
 	else if (n == 0)
 	{
-		printf("is zero\n");
+		printf("%d is zero\n", n);
 	}
 	/* Return 0 if successful */
 	return (0);
