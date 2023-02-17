da9598a8134be373656c92918a3431bc50ca6fee
simonmuia <simonmuia38@gmail.com>
2023-02-17 05:25:20 -0800

    combo of double two numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 			putchar(' ');
 			putchar((snum / 10) + '0');
 			putchar((snum % 10) + '0');
-			if (i != 98 || j != 99)
+			if (fnum != 98 || snum != 99)
 			{
 				putchar(',');
 				putchar(' ');
