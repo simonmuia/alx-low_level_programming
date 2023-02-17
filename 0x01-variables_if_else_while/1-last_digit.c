2561cce093c1edb576d7271228833e99cd1ec09a
simonmuia <simonmuia38@gmail.com>
2023-02-17 01:59:08 -0800

    pick the last digit of a random number

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,20 +11,19 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	int last_digit = n % 10;
+	n = rand(n%10) - RAND_MAX / 2;
 
-	if (last_digit > 5)
+	if (n > 5)
 	{
-		printf("%d and is greater than 5\n", last_digit);
+		printf("%d and is greater than 5\n", n);
 	}
-	else if (last_digit == 0)
+	else if (n == 0)
 	{
-		printf("%d and is 0\n", last_digit);
+		printf("%d and is 0\n", n);
 	}
 	else
 	{
-		printf("%d and is less than 6 and not 0\n", last_digit);
+		printf("%d and is less than 6 and not 0\n", n);
 	}
 	return (0);
 
