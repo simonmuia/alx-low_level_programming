48e0dc0e835da7dc5151c22ff87fc7d9319a639f
simonmuia <simonmuia38@gmail.com>
2023-02-21 11:11:21 -0800

    print sequence from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers to 98
+ * @n: input variable
+ * Return: 0
+ */
+
+void print_to_98(int n)
+{
+	int number;
+
+	if (n > 98)
+	{
+		for (number = 98; number <= n; number--)
+		{
+			_putchar(number);
+
+			if (number != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else if (n < 98)
+	{
+		for (number = 0; number <= 98; number++)
+		{
+			_putchar(number);
+			if (number != 98)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+	}
+	else
+		_putchar(n);
+}
