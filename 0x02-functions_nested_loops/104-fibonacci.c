7893d1c73da13d4a1261689789932b611f7cabf2
simonmuia <simonmuia38@gmail.com>
2023-02-21 13:33:32 -0800

    print first 98 fib numbers

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int count, boolean, boolean2;
+	long int n1, n2, fn, fn2, n11, n22;
+
+	n1 = 1;
+	n2 = 2;
+	boolean =  boolean2 = 1;
+	printf("%ld, %ld", n1, n2);
+	for (count = 0; count < 96; count++)
+	{
+		if (boolean)
+		{
+			fn = n1 + n2;
+			printf(", %ld", fn);
+			n1 = n2;
+			n2 = fn;
+		}
+		else
+		{
+			if (boolean2)
+			{
+				n11 = n1 % 1000000000;
+				n22 = n2 % 1000000000;
+				n1 = n1 / 1000000000;
+				n2 = n2 / 1000000000;
+				boolean2 = 0;
+			}
+			fn2 = (n11 + n22);
+			fn = n1 + n2 + (fn2 / 1000000000);
+			printf(", %ld", fn);
+			printf("%ld", fn2 % 1000000000);
+			n1 = n2;
+			n11 = n22;
+			n2 = fn;
+			n22 = (fn2 % 1000000000);
+		}
+		if (((n1 + n2) < 0) && boolean == 1)
+			boolean = 0;
+	}
+	printf("\n");
+	return (0);
+}
