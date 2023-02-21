66e978509269d3caeb69bc97635d79b95651e7de
simonmuia <simonmuia38@gmail.com>
2023-02-21 09:05:15 -0800

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,10 @@
 
 int print_last_digit(int num)
 {
-	int last_digit = num % 10;
+	int ld = num % 10;
 
-	if (last_digit < 0)
-		last_digit *= -1;
-	_putchar(last_digit + '0');
-	return (0);
+	if (ld < 0)
+		ld *= -1;
+	_putchar(ld + '0');
+	return (ld < 0 ? -1 * ld : ld);
 }
