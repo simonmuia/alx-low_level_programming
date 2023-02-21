8ebee7a27c60ea898bcca5cd5f6bdd889df98476
simonmuia <simonmuia38@gmail.com>
2023-02-21 07:11:10 -0800

    print alphabets x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int instances;
 	char c;
 
-	for (instances = 0; instances <= 10; instances++)
+	for (instances = 0; instances < 10; instances++)
 	{
 		for (c = 'a'; c <= 'z'; c++)
 		{
