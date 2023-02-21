4743f6f4da51ebc94e698ca16c3266f9d50cabd5
simonmuia <simonmuia38@gmail.com>
2023-02-21 04:46:47 -0800

    Fix putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,16 +4,10 @@
 #include <unistd.h>
 
 /**
- * _putchar - custom library.
+ * print _putchar - custom library.
  *
  * Return: on success 1
  */
-
-int _putchar(char c)
-{
-	return(write(1, &c, 1));
-}
-
 void print_putchar(void)
 {
 	puts("_putchar");
