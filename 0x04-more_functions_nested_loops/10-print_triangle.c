d0e6acc535e8c855e2e62068340611e08783c9ad
simonmuia <simonmuia38@gmail.com>
2023-02-23 04:13:29 -0800

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,12 +9,12 @@
 void print_triangle(int size)
 {
 	int space, count, hash;
-	
+
 	if (size <= 0)
 		_putchar('\n');
 	for (count = 0; count < size; count++)
 	{
-		for (space = (size-count); space >= 1; space--)
+		for (space = (size - count); space >= 1; space--)
 			_putchar(' ');
 		for (hash = 1; hash <= count; hash++)
 			_putchar('#');
