af447a7e4ed594ea75c859ceb8e9c9b30e71185c
simonmuia <simonmuia38@gmail.com>
2023-02-22 22:07:09 -0800

    print numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0-9 followed by new line.
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n + 48);
+	}
+	_putchar("\n");
+}
