e09165747d41aca74dd4696935ac179526d76eaa
simonmuia <simonmuia38@gmail.com>
2023-02-23 03:56:39 -0800

    print fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,23 +12,25 @@ int main(void)
 
 	for (num = 1; num <= count; num++)
 	{
-		if ((num % 3) == 0)
+		if (num % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
-		else if ((num % 5) == 0)
+		else if (num % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
-		else if ((num % 15) == 0)
+		else if (num % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else
 		{
 			printf("%d ", num);
 		}
+		if (num < 100)
+			printf(" ");
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
