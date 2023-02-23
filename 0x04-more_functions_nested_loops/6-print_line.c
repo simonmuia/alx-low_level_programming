5d9fa74bd5f1f135480fc76ba0e14916bf448ea5
simonmuia <simonmuia38@gmail.com>
2023-02-22 22:46:45 -0800

    print line n times

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line in terminal.
+ * @n: variable input for number of times character _ should be printed.
+ *
+ */
+
+void print_line(int n)
+{
+	int count = 1;
+
+	while (count <= n)
+	{
+		if (n <= 0)
+		{
+			_putchar("\n");
+		}
+		_putchar("_");
+		count++;
+	}
+	_putchar("\n");
+}
