10e28ce8e7c40583a005b6b4c54aae4960f55519
simonmuia <simonmuia38@gmail.com>
2023-02-23 00:10:47 -0800

    print fizz buzz fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - print nums conditionally
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int count = 100, num;
+
+	for (num = 1; num <= count; num++)
+	{
+		if ((num % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else if ((num % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		else if ((num % 15) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", num);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
