4f8179c5bf9bd9d2ab94914efddb2d81629ccc61
simonmuia <simonmuia38@gmail.com>
2023-02-23 04:04:43 -0800

    print fizz buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,26 +8,18 @@
 
 int main(void)
 {
-	int count = 100, num;
+	int num;
 
-	for (num = 1; num <= count; num++)
+	for (num = 1; num <= 100; num++)
 	{
 		if (num % 3 == 0)
-		{
 			printf("Fizz");
-		}
 		else if (num % 5 == 0)
-		{
 			printf("Buzz");
-		}
 		else if (num % 15 == 0)
-		{
 			printf("FizzBuzz");
-		}
 		else
-		{
 			printf("%i", num);
-		}
 		if (num < 100)
 			printf(" ");
 	}
