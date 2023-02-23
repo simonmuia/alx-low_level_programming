fce6e7c576fde054dc462493b4736bc3c63460f3
simonmuia <simonmuia38@gmail.com>
2023-02-23 00:41:58 -0800

    print diagonal line

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,16 +8,19 @@
 
 void print_diagonal(int n)
 {
-	int count = 1;
+	int line_count = 0, space_count = 0;
 
-	while (count <= n)
+	for (line_count = 0; line_count < n; line_count++)
 	{
-		if (n <= 0)
+		if (n == 0)
 		{
 			_putchar("\n");
 		}
+		for (space_count = 0; space_count < line_count; space_count++)
+		{
+			_putchar(" ");
+		}
 		_putchar("\\");
-		count++;
+		_putchar("\n");
 	}
-	_putchar("\n");
 }
