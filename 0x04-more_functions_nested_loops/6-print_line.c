17cbffe2574cf6a77452f17cdf9a053114cdee1e
simonmuia <simonmuia38@gmail.com>
2023-02-23 02:59:03 -0800

    print line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -14,10 +14,10 @@ void print_line(int n)
 	{
 		if (n <= 0)
 		{
-			_putchar("\n");
+			_putchar('\n');
 		}
-		_putchar("_");
+		_putchar('_');
 		count++;
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
