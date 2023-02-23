e2e0c2c5e0fe7f5c45da3ef38bcc3cc984e1b3c1
simonmuia <simonmuia38@gmail.com>
2023-02-22 22:24:42 -0800

    print 10 times numbers range 0-9

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0-14 10 times.
+ *
+ */
+
+void more_numbers(void)
+{
+	int n, times;
+
+	for (times = 0; times <= 10; times++)
+	{
+		for (n = 0; n < 15; n++)
+		{
+			_putchar(n);
+		}
+		_putchar("\n");
+	}
+	_putchar("\n");
+}
