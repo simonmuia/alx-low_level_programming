0e595234624022ee09050317ba2ef9843dca2587
simonmuia <simonmuia38@gmail.com>
2023-02-27 06:48:59 -0800

    prints string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - prints string followed by new line to stdout
+ *
+ */
+
+void _puts(*char *str);
+{
+	int letter;
+
+	for (letter = 0; letter <= *str; *str++)
+		_putchar(*str);
+	_putchar('\n');
+}
