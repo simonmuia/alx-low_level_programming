65b25777f9a8c0ef1624c1b308a434ccc8dd5248
simonmuia <simonmuia38@gmail.com>
2023-02-27 08:48:17 -0800

    prints every other character

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,7 +9,7 @@ void puts2(char *str)
 {
 	int count;
 
-	for (count = 0; str[count] != 0 '\0'; count++)
+	for (count = 0; str[count] != '\0'; count++)
 	{
 		if (count % 2 == 0)
 			_putchar(str[count]);
