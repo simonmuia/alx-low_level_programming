045341f7c8316068e33940010160b99521dff3aa
simonmuia <simonmuia38@gmail.com>
2023-02-27 07:21:36 -0800

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse
+ * @s: string variable
+ */
+
+void print_rev(char *s)
+{
+	int count, len;
+
+	for (len = 0; s[len] != 0; count++)
+	{
+		for (count = len - 1; count >= 0; count--)
+		{
+			_putchar(s[count]);
+		}
+	}
+	_putchar('\n');
+}
