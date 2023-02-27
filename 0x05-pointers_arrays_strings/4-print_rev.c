0c0b4609048fa07ca09a2ef1d6a650b37f41ea18
simonmuia <simonmuia38@gmail.com>
2023-02-27 07:40:32 -0800

    print string reversed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,11 @@ void print_rev(char *s)
 {
 	int count, len;
 
-	for (len = 0; s[len] != '\0'; len++)
+	while (s[len] != '\0')
+		len++;
+	for (count = len - 1; count >= 0; count--)
 	{
-		for (count = len - 1; count >= 0; count--)
-		{
-			_putchar(s[count]);
-		}
+		_putchar(s[count]);
 	}
 	_putchar('\n');
 }
