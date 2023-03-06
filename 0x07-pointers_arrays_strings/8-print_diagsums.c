4a72d6e91b27115870ed79abcc377fa6a73bab3c
simonmuia <simonmuia38@gmail.com>
2023-03-06 11:21:43 -0800

    print two diagonals

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -7,16 +7,17 @@
  *
  * Return: void
  */
-void print_diagsums(int *matrix, int size)
+
+void print_diagsums(int *a, int size)
 {
 	int i, sum_main = 0, sum_secondary = 0;
 
 	for (i = 0; i < (size * size); i++)
 	{
 		if (i % (size + 1) == 0)
-			sum_main += *(matrix + i);
+			sum_main += *(a + i);
 		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-			sum_secondary += *(matrix + i);
+			sum_secondary += *(a + i);
 	}
 	printf("%d, %d\n", sum_main, sum_secondary);
 }
