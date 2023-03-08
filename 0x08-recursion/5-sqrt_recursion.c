7cc73e8151330030089451a3499c8163345baf0d
simonmuia <simonmuia38@gmail.com>
2023-03-07 22:43:22 -0800

    squareroot of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -4,8 +4,8 @@
  * squareroot - returns actual squareroot of a given number
  * @n: given number
  * @i: iterator.
+ * Return: recursion.
  */
-
 int squareroot(int n, int i);
 
 /**
@@ -21,11 +21,18 @@ int _sqrt_recursion(int n)
 	return (squareroot(n, 0));
 }
 
+/**
+ * squareroot - prints squareroot of given number.
+ * @n: given number
+ * @i : iterator.
+ * Return: recursion of the number.
+ */
+
 int squareroot(int n, int i)
 {
 	if (i * i > n)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (squareroot (n, i + 1));
+	return (squareroot(n, i + 1));
 }
