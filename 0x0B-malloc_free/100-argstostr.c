2e60be9ec32ea0e4e14167c07132d29b694073e7
simonmuia <simonmuia38@gmail.com>
2023-03-13 07:11:24 -0700

    concatenates all arguments in program

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -14,28 +14,28 @@ char *argstostr(int ac, char **av)
 	char *str;
 	int i, j, len = 0, pos = 0;
 
-	if (ac <= 0 || av == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
 	/* Calculate the total length of the arguments */
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 			len++;
 	}
-	len += ac;
+	pos += ac;
 
 	/* Allocate memory for the concatenated string */
-	str = malloc(sizeof(char) * len);
+	str = malloc(sizeof(char) * pos + 1);
 	if (str == NULL)
 		return (NULL);
 
 	/* Copy the arguments into the concatenated string */
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 		{
-			str[pos++] = av[i][j];
+			str[len] = av[i][j];
 			j++;
 		}
 		if (str[k] == '\0')
