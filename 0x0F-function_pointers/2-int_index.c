0803ce95cb086ff61fe1122b9c239191b6c86f68
Simon Muia <simonmuia38@gmail.com>
2023-03-22 13:22:33 +0300

    searches for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,6 +12,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 if (array && cmp)
+{
 if (size <= 0)
 return (-1);
 for (i = 0; i < size; i++)
@@ -19,5 +20,6 @@ for (i = 0; i < size; i++)
 if (cmp(array[i]))
 return (i);
 }
-return (-1)
+}
+return (-1);
 }
