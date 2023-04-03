6dc504e3eac77c2a3b8fad83518acdbd551be74a
Simon Muia <simonmuia38@gmail.com>
2023-04-03 13:39:44 +0300

    adds a new node at beginning of a list

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at beginning of 
- * a list
+ * add_nodeint - adds a new node at beginning of a list
  * @head: head of a list.
  * Return: Address of a new element and NULL if fails
+ * @n: number of nodes
 */
 
 listint_t *add_nodeint(listint_t **head, const int n)
