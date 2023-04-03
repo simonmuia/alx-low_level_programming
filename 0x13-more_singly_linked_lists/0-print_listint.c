46b895c0e581e458d824dc0ff820d588079d572b
Simon Muia <simonmuia38@gmail.com>
2023-04-03 11:12:01 +0300

    print all elements of a list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all elements of a list
+ * @h: pointer to elements of a list
+ * Return: null if elements are empty
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	size_t elsize;
+
+	elsize = 0;
+	while(h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		elsize++;
+	}
+	return (elsize);
+}
