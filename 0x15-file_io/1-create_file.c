d27d6a5dd6edcfa941ee3d3f5b70043cc7fadc20
Simon Muia <simonmuia38@gmail.com>
2023-04-10 14:27:32 +0300

    creates file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * create_file - creates file
+ * @filename: filename string
+ * @text_content: content of a file
+ * Return: -1 if filename is NULL
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int file, num_lets, file_write;
+
+	if (!filename)
+		return (-1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (file == -1)
+		return (-1);
+	
+	if (!text_content)
+		text_content = "";
+	
+	for (num_lets = 0; text_content[num_lets]; num_lets++)
+		;
+	file_write = write(filename, text_content, num_lets);
+
+	if (file_write == -1)
+		return (-1);
+	
+	close(file);
+	return (1);
+}
