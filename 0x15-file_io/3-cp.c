054da30837038cc7f052e3a8743a6a95a839d356
Simon Muia <simonmuia38@gmail.com>
2023-04-10 19:16:39 +0300

    copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,7 +36,8 @@ int main(int argc, char *argv[])
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664), error_handler(-1, file_to, argv);
 
 	while ((num_c = read(file_from, buf, 1024)))
-		num_c == -1 ? error_handler(-1, 0, argv) : write(file_to, buf, num_c) == -1 ? error_handler(0, -1, argv) : (void)0;
+		num_c == -1 ? error_handler(-1, 0, argv) : write(file_to, buf, num_c) == -1 ? error_handler(0, -1, argv)
+																					: (void)0;
 
 	close_err_handler = close(file_from), close_err_handler == -1 ? dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from), exit(100) : (void)0;
 	close_err_handler = close(file_to), close_err_handler == -1 ? dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100) : (void)0;
