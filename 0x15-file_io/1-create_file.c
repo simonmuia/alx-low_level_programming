a52625c9c7414c492ae044e5ef459046fd4224f5
Simon Muia <simonmuia38@gmail.com>
2023-04-10 14:29:27 +0300

    creates file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,13 +8,13 @@
 
 int create_file(const char *filename, char *text_content)
 {
-	int file, num_lets, file_write;
+	int _file, num_lets, file_write;
 
 	if (!filename)
 		return (-1);
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (file == -1)
+	if (_file == -1)
 		return (-1);
 
 	if (!text_content)
@@ -27,6 +27,6 @@ int create_file(const char *filename, char *text_content)
 	if (file_write == -1)
 		return (-1);
 
-	close(file);
+	close(_file);
 	return (1);
 }
