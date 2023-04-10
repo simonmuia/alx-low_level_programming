f047d1241e4e07392691bf3388c7f4ad772c1660
Simon Muia <simonmuia38@gmail.com>
2023-04-11 02:49:37 +0300

    display information on ELF header

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -291,10 +291,10 @@ void elf_close_handler(int elf)
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	Elf64_Ehdr *header;
-	int o, r;
+	int file_o, file_read;
 
-	o = open(argv[1], O_RDONLY);
-	if (o == -1)
+	file_o = open(argv[1], O_RDONLY);
+	if (file_o == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
@@ -302,15 +302,15 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	header = malloc(sizeof(Elf64_Ehdr));
 	if (header == NULL)
 	{
-		elf_close_handler(o);
+		elf_close_handler(file_o);
 		dprintf(STDERR_FILENO, "Error: Can't read file %s\n", argv[1]);
 		exit(98);
 	}
-	r = read(o, header, sizeof(Elf64_Ehdr));
-	if (r == -1)
+	file_read = read(file_o, header, sizeof(Elf64_Ehdr));
+	if (file_read == -1)
 	{
 		free(header);
-		close_elf(o);
+		elf_close_handler(file_o);
 		dprintf(STDERR_FILENO, "Error: `%s`: No such file\n", argv[1]);
 		exit(98);
 	}
@@ -327,6 +327,6 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	elf_entry_handler(header->e_entry, header->e_ident);
 
 	free(header);
-	elf_close_handler(o);
+	elf_close_handler(file_o);
 	return (0);
 }
