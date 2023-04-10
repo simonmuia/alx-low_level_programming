f7f6dbdc5c3e9f9954e1d1cc0922567ad729434d
Simon Muia <simonmuia38@gmail.com>
2023-04-10 19:04:52 +0300

    copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,15 +12,9 @@ void error_handler(int source_file, int dest_file, char *argv[])
 {
 
 	if (source_file == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n ", argv[1]);
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
 	if (dest_file == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n ", argv[2]);
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 }
 
 /**
@@ -36,36 +30,16 @@ int main(int argc, char *argv[])
 	char buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	source_file= open(argv[1], O_RDONLY);
-	
+	source_file = open(argv[1], O_RDONLY), error_handler(source_file, -1, argv);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664), error_handler(-1, file_to, argv);
 
-	for (num_c = 1024; num_c == 1024;)
-	{
-		num_c = read(source_file, buf, 1024);
-		if (num_c == -1)
-			error_handler(-1, 0, argv);
-		file_write = write(file_to, buf, num_c);
-		if (file_write == -1)
-			error_handler(0, -1, argv);
-	}
+	while ((num_c = read(source_file, buf, 1024)))
+		num_c == -1 ? error_handler(-1, 0, argv) : write(file_to, buf, num_c) == -1 ? error_handler(0, -1, argv) : (void)0;
 
-	close_err_handler = close(source_file);
-	if (close_err_handler == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close _file %d\n", source_file);
-		exit(100);
-	}
+	close_err_handler = close(source_file), close_err_handler == -1 ? dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", source_file), exit(100) : (void)0;
+	close_err_handler = close(file_to), close_err_handler == -1 ? dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_to), exit(100) : (void)0;
 
-	close_err_handler = close(file_to);
-	if (close_err_handler == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close _file %d\n", source_file);
-		exit(100);
-	}
 	return (0);
 }
