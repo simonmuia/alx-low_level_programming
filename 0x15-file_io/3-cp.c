e99e6454e9a347c638adf9e350bc5d47b4f078ae
Simon Muia <simonmuia38@gmail.com>
2023-04-10 19:22:09 +0300

    copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 
 /**
- * error_file - checks if files can be opened.
+ * error_handler - checks if files can be opened.
  * @file_from: file_from.
  * @file_to: file_to.
  * @argv: arguments vector.
  *
  * Return: no return.
  */
-void error_file(int file_from, int file_to, char *argv[])
+void error_handler(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
 	{
@@ -32,7 +32,7 @@ void error_file(int file_from, int file_to, char *argv[])
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, err_close;
+	int file_from, file_to, close_error_handler;
 	ssize_t nchars, nwr;
 	char buf[1024];
 
@@ -44,27 +44,27 @@ int main(int argc, char *argv[])
 
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
+	error_handler(file_from, file_to, argv);
 
 	do
 	{
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
-			error_file(-1, 0, argv);
+			error_handler(-1, 0, argv);
 		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
-			error_file(0, -1, argv);
+			error_handler(0, -1, argv);
 	} while (nchars == 1024);
 
-	err_close = close(file_from);
-	if (err_close == -1)
+	close_error_handler = close(file_from);
+	if (close_error_handler == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	err_close = close(file_to);
-	if (err_close == -1)
+	close_error_handler = close(file_to);
+	if (close_error_handler == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
