8a0a5f70b1cec88f18be784a17c913d3bc5f051b
Simon Muia <simonmuia38@gmail.com>
2023-04-10 18:01:58 +0300

    copies content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -32,7 +32,7 @@ int main(int argc, char *argv[])
 {
 	int file_from, file_to, close_err_handler;
 	ssize_t num_c, file_write;
-	char buffer[1024];
+	char *buf;
 
 	if (argc != 3)
 	{
@@ -40,16 +40,17 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
+	buf = create_buffer(argv[2]);
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY | O_APPEND, 0664);
 	error_file(file_from, file_to, argv);
 
 	while (num_c == 1024)
 	{
-		num_c = read(file_from, buffer, 1024);
+		num_c = read(file_from, buf, 1024);
 		if (num_c == -1)
 			error_handler(-1, 0, argv);
-		file_write = write(file_to, buffer, num_c);
+		file_write = write(file_to, buf, num_c);
 		if (file_write == -1)
 			error_handler(0, -1, argv);
 	}
