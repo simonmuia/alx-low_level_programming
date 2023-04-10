78cbdd2ff269f7d91dc8f6ca909ae46bc51b3d31
Simon Muia <simonmuia38@gmail.com>
2023-04-10 12:07:05 +0300

    converts binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,18 +8,15 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	/* Initialize results and iterator*/
 	unsigned int convNum = 0;
 	int str_len, i;
 
 	if (!b)
 		return (0);
 
-	/* find string length */
 	for (str_len = 0; b[str_len] != '\0'; str_len++)
 		;
 
-	/*convert input to unsigned int*/
 	for (str_len--, i = 1; str_len >= 0; str_len--, i *= 2)
 	{
 		if (b[str_len] != '0' && b[str_len] != '1')
